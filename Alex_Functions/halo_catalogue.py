I should know better than to get the unit conversion the wrong way around

Alex_Functions/halo_catalogue.py
@@ -150,7 +150,7 @@ class FlamingoSnapshot(HaloCatalogue):
 
         h = used_params["Cosmology"]["h"]
         L = path_config["Params"]["L"]
-        self.box_size = L / h
+        self.box_size = L * h
         snapshot_redshift = path_config["Params"]["redshift"]
         particles = path_config["Params"]["particles"]
 
@@ -160,7 +160,7 @@ class FlamingoSnapshot(HaloCatalogue):
             halo_type = "soap"
 
         UnitMass_in_cgs = float(used_params["InternalUnitSystem"]["UnitMass_in_cgs"])
-        UnitMass_in_Msol_h = UnitMass_in_cgs / (1.98841e33 * h)
+        UnitMass_in_Msol_h = UnitMass_in_cgs * h / 1.98841e33
 
         # read SOAP halo catalogue file
 
@@ -196,10 +196,10 @@ class FlamingoSnapshot(HaloCatalogue):
             relevant_field_halos = np.logical_and(relevant_field_halos, is_nonzero_rvmax)
             
             self._quantities = {
-                'pos':   np.array(halo_cat["SO"]["200_crit"]["CentreOfMass"])[relevant_field_halos] / h,
+                'pos':   np.array(halo_cat["SO"]["200_crit"]["CentreOfMass"])[relevant_field_halos] * h,
                 'vel':   np.array(halo_cat["SO"]["200_crit"]["CentreOfMassVelocity"])[relevant_field_halos],
                 'mass':  np.array(halo_cat["SO"]["200_crit"]["DarkMatterMass"])[relevant_field_halos] * UnitMass_in_Msol_h,
-                'rvmax': np.array(halo_cat["BoundSubhalo"]["MaximumDarkMatterCircularVelocityRadius"])[relevant_field_halos] / h
+                'rvmax': np.array(halo_cat["BoundSubhalo"]["MaximumDarkMatterCircularVelocityRadius"])[relevant_field_halos] * h
             }
         elif halo_type == "peregrinus":
             is_not_subhalo = np.array(halo_cat["Subhalos"]["Rank"]) == 0
@@ -207,10 +207,10 @@ class FlamingoSnapshot(HaloCatalogue):
             is_not_0mass = np.array(halo_cat["Subhalos"]["BoundM200Crit"]) != 0
             relevant_field_halos = np.logical_and(is_not_0mass, is_not_subhalo)
             self._quantities = {
-                'pos':   np.array(halo_cat["Subhalos"]["ComovingAveragePosition"])[relevant_field_halos] / h,
+                'pos':   np.array(halo_cat["Subhalos"]["ComovingAveragePosition"])[relevant_field_halos] * h,
                 'vel':   np.array(halo_cat["Subhalos"]["PhysicalAverageVelocity"])[relevant_field_halos],
                 'mass':  np.array(halo_cat["Subhalos"]["BoundM200Crit"])[relevant_field_halos] * UnitMass_in_Msol_h,
-                'rvmax': np.array(halo_cat["Subhalos"]["RmaxComoving"])[relevant_field_halos] / h
+                'rvmax': np.array(halo_cat["Subhalos"]["RmaxComoving"])[relevant_field_halos] * h
             }
 
         self.size = len(self._quantities['mass'][...])

Alex_Functions/read_hdf5.py
@@ -18,7 +18,7 @@ def read_soap_log_mass(input_file, UnitMass_in_cgs, h):
     is_not_0mass = np.array(halo_cat["SO"]["200_crit"]["DarkMatterMass"]) != 0
     relevant_field_halos = np.logical_and(is_not_0mass, is_not_subhalo)
 
-    UnitMass_in_Msol_h = UnitMass_in_cgs / (1.98841e33 * h)
+    UnitMass_in_Msol_h = UnitMass_in_cgs * h / 1.98841e33
     log_mass = np.log10(np.array(halo_cat["SO"]["200_crit"]["DarkMatterMass"])[relevant_field_halos] * UnitMass_in_Msol_h)
     return log_mass
 
@@ -36,7 +36,7 @@ def read_hbt_log_mass(input_file, UnitMass_in_cgs, h):
     is_not_0mass = np.array(halo_cat["Subhalos"]["BoundM200Crit"]) != 0
     relevant_field_halos = np.logical_and(is_not_0mass, is_not_subhalo)
 
-    UnitMass_in_Msol_h = UnitMass_in_cgs / (1.98841e33 * h)
+    UnitMass_in_Msol_h = UnitMass_in_cgs * h / 1.98841e33
     log_mass = np.log10(np.array(halo_cat["Subhalos"]["BoundM200Crit"])[relevant_field_halos] * UnitMass_in_Msol_h)
     return log_mass
 
@@ -92,7 +92,7 @@ def get_log_min_halo_mass(path_config_filename):
         params = yaml.safe_load(file)
 
     unit_mass = params["Snapshots"]["UnitMass_in_cgs"] / 1.98841e33
-    particle_mass_Msol_h = get_average_dm_particle_mass(path_config_filename) * unit_mass / params["Cosmology"]["h"]
+    particle_mass_Msol_h = get_average_dm_particle_mass(path_config_filename) * unit_mass * params["Cosmology"]["h"]
     
     if halo_type == "soap":
         halo_cat = h5py.File(soap_path, "r")