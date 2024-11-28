62bd213dae093efb543d8677c96ef17dfc05e144
Alexander M. <rtg142857@gmail.com>
2024-11-28 15:52:02 +0000

    Greatly increasing batch time

--- batch_hod_fitting.sh
+++ batch_hod_fitting.sh
@@ -9,7 +9,7 @@
 #SBATCH -p cosma8 #or some other partition, e.g. cosma, cosma8, etc.
 #SBATCH -A dp004
 #SBATCH --exclusive
-#SBATCH -t 300
+#SBATCH -t 1440
 #SBATCH --mail-type=ALL # notifications for job done & fail
 #SBATCH --mail-user=tlrt88@durham.ac.uk #PLEASE PUT YOUR EMAIL ADDRESS HERE (without the <>)
 
